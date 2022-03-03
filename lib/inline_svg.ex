b4a2a5cc96b2d306dd030818a58be6bb2e13c3c9
c4710n <c4710n@users.noreply.github.com>
2022-03-03 23:28:20 +0800

    init: `mix new ex_inline_svg --module InlineSVG`

---
+++ lib/inline_svg.ex
@@ -0,0 +1,18 @@
+defmodule InlineSVG do
+  @moduledoc """
+  Documentation for `InlineSVG`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> InlineSVG.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/inline_svg_test.exs
@@ -0,0 +1,8 @@
+defmodule InlineSVGTest do
+  use ExUnit.Case
+  doctest InlineSVG
+
+  test "greets the world" do
+    assert InlineSVG.hello() == :world
+  end
+end
