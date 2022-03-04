425608b4579d32a0b24f8364b8a6054cea524c3d
c4710n <c4710n@users.noreply.github.com>
2022-03-04 17:00:24 +0800

    chore: bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule InlineSVG.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @source_url "https://github.com/c4710n/ex_inline_svg"
   @description "Just an inline SVG file renderer."
 
