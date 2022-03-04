7efbac1685c35a07b29b63a9ab852595af4a38e6
c4710n <c4710n@users.noreply.github.com>
2022-03-04 16:56:18 +0800

    chore: prepare release

--- README.md
+++ README.md
@@ -6,19 +6,10 @@
 - high performance - parse and cache SVG at compile time, instead of runtime
 - doesn't bind to Phoenix ecosystem
 
-## Installation
+## Usage
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `inline_svg` to your list of dependencies in `mix.exs`:
+Visit [HexDocs](https://hexdocs.pm/phx_custom) for more details.
 
-```elixir
-def deps do
-  [
-    {:inline_svg, "~> 0.1.0"}
-  ]
-end
-```
+## License
 
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at <https://hexdocs.pm/inline_svg>.
+Apache License 2.0
--- mix.exs
+++ mix.exs
@@ -1,13 +1,22 @@
 defmodule InlineSVG.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/c4710n/ex_inline_svg"
+  @description "Just an inline SVG file renderer."
+
   def project do
     [
       app: :inline_svg,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: @description,
+      homepage_url: @source_url,
+      package: package(),
+      aliases: aliases(),
+      docs: docs()
     ]
   end
 
@@ -21,7 +30,35 @@ defmodule InlineSVG.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:floki, "~> 0.32"}
+      {:floki, "~> 0.32"},
+      {:ex_doc, "~> 0.28", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      exclude_patterns: [],
+      licenses: ["Apache-2.0"],
+      links: %{GitHub: @source_url}
+    ]
+  end
+
+  defp aliases do
+    [publish: ["hex.publish", "tag"], tag: &tag_release/1]
+  end
+
+  defp tag_release(_) do
+    Mix.shell().info("Tagging release as #{@version}")
+    System.cmd("git", ["tag", @version])
+    System.cmd("git", ["push", "--tags"])
+  end
+
+  defp docs do
+    [
+      extras: ["README.md"],
+      main: "readme",
+      source_url: @source_url,
+      source_ref: @version
     ]
   end
 end
